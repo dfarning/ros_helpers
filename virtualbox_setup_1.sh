a799486077e9b5da744e57c1b6b13cde3c4add2b
David Farning <dfarning@gmail.com>
2021-04-22 18:28:15 -0500

    update scripts

---
+++ virtualbox_setup_1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo cp 01proxy /etc/apt/apt.conf.d/01proxy
+sudo apt install build-essential
+
+# Insert Guest Additions CD
---
+++ vs_code_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install git
+
+
+git config --global user.name "David Farning"
+git config --global user.email "dfarning@gmail.com"
