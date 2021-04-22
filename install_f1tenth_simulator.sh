a221b173c124b40ad04a3db7cc92aa68288c96ad
David Farning <dfarning@gmail.com>
2021-04-22 05:18:30 -0500

    update install scripts

--- install_f1tenth_simulator.sh
+++ install_f1tenth_simulator.sh
@@ -1,16 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+# Apache License 2.0
+# Copyright (c) 2018, ROBOTIS CO., LTD.
+# Modifications copyright (C) 2021 dfarning@gmail.com
+
+echo ""
+echo "[Note] Target OS version  >>> Ubuntu 18.04.x (Bionic Beaver)"
+echo "[Note] Target ROS version >>> ROS Melodic Morenia"
+echo "[Note] Catkin workspace   >>> $HOME/autosim_ws"
+echo ""
+echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
+echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
+read
+
+echo "[Set the target OS, ROS version and name of catkin workspace]"
+name_os_version=${name_os_version:="bionic"}
+name_ros_version=${name_ros_version:="melodic"}
+name_catkin_workspace=${name_catkin_workspace:="autosim_ws"}
 
 sudo apt-get install -y ros-melodic-navigation ros-melodic-teb-local-planner* ros-melodic-ros-control \
     ros-melodic-ros-controllers ros-melodic-gazebo-ros-control ros-melodic-ackermann-msgs ros-melodic-serial qt4-default
 
-mkdir -p ~/autosim_ws/src
-cd ~/autosim_ws/src
+echo "[Make the catkin workspace and test the catkin_make]"
+mkdir -p $HOME/$name_catkin_workspace/src
+cd $HOME/$name_catkin_workspace/src
 catkin_init_workspace
+cd $HOME/$name_catkin_workspace
+catkin_make
 
+cd $HOME/$name_catkin_workspace/src
 git clone https://github.com/f1tenth-dev/simulator
 git clone https://github.com/mit-racecar/particle_filter
 git clone https://github.com/kctess5/range_libc
 git clone https://github.com/tu-darmstadt-ros-pkg/hector_slam
 
-sudo apt install -y python-pip
-sudo -H pip install cython
+#sudo apt install -y python-pip
+#sudo -H pip install cython
--- install_ros_melodic.sh
+++ install_ros_melodic.sh
@@ -79,11 +79,11 @@ echo "[Set the ROS evironment]"
 # sh -c "echo \"alias cs='cd ~/$name_catkin_workspace/src'\" >> ~/.bashrc"
 # sh -c "echo \"alias cm='cd ~/$name_catkin_workspace && catkin_make'\" >> ~/.bashrc"
 
-sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
-# sh -c "echo \"source ~/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"
+# sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
+sh -c "echo \"source ~/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"
 
-# sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
-# sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
+sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
+sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
 
 source $HOME/.bashrc
 
