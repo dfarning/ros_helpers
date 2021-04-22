d3ad5363875f069d766cfede8f53281701b1dad2
David Farning <dfarning@gmail.com>
2021-04-22 07:38:45 -0500

    update the compile script

--- compile_f1tenth_simulator.sh
+++ compile_f1tenth_simulator.sh
@@ -1,8 +1,24 @@
-#!/bin/sh
+#!/bin/bash
+# Apache License 2.0
+# Copyright (c) 2018, ROBOTIS CO., LTD.
+# Modifications copyright (C) 2021 dfarning@gmail.com
 
-cd ~/autosim_ws/src/range_libc/pywrapper
+echo ""
+echo "[Note] Target OS version  >>> Ubuntu 18.04.x (Bionic Beaver)"
+echo "[Note] Target ROS version >>> ROS Melodic Morenia"
+echo "[Note] Catkin workspace   >>> $HOME/autosim_ws"
+echo ""
+echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
+echo "IF YOU WANT TO CANCEL, PRESS [CTRL] + [C]"
+read
 
+echo "[Set the target OS, ROS version and name of catkin workspace]"
+name_os_version=${name_os_version:="bionic"}
+name_ros_version=${name_ros_version:="melodic"}
+name_catkin_workspace=${name_catkin_workspace:="autosim_ws"}
+
+cd $HOME/$name_catkin_workspace/src/range_libc/pywrapper
 ./compile.sh
 
-cd ~/autosim_ws
+cd $HOME/$name_catkin_workspace
 catkin_make install
